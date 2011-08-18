bf15f436c930a66ac4ec5907fb893b18ac9cf8ba
Michael Leinartas <mleinartas@gmail.com>
2011-08-18 14:29:35 -0500

    Add slf4j and 3 implementations - log4j, logback, and java.util.logging Keep existing behavior - default to log4j with config/trinidad-logging.properties as default

---
+++ lib/trinidad_logging_extension/jars.rb
@@ -0,0 +1,33 @@
+$:.unshift(File.expand_path"../../trinidad-libs", File.dirname(__FILE__))
+
+module Trinidad
+  module Extensions
+    module LoggingJars
+      SLF4J_VERSION = '1.6.1'
+      LOG4J_VERSION = '1.2.16'
+      LOGBACK_VERSION = '0.9.29'
+
+      def require_common_jars
+        require "slf4j-api-#{SLF4J_VERSION}.jar"
+        require "juli-adapters.jar"
+        require "jcl-over-slf4j-#{SLF4J_VERSION}.jar"
+      end
+
+      def require_log4j_jars
+        require "slf4j-log4j12-#{SLF4J_VERSION}.jar"
+        require "log4j-#{LOG4J_VERSION}"
+        require "jul-to-slf4j-#{SLF4J_VERSION}.jar"
+      end
+
+      def require_logback_jars
+        require "logback-core-#{LOGBACK_VERSION}.jar"
+        require "logback-classic-#{LOGBACK_VERSION}.jar"
+        require "jul-to-slf4j-#{SLF4J_VERSION}.jar"
+      end
+
+      def require_jul_jars
+        require "slf4j-jdk14-#{SLF4J_VERSION}.jar"
+      end
+    end
+  end
+end
