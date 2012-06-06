33f8d121979465c837250e6ef3b97a1e0f1ca09e
Robert Rasmussen <robert@spiceworks.com>
2012-06-06 12:44:46 -0500

    Add log4j extras so we can use RollingFileAppender

--- lib/trinidad_logging_extension/jars.rb
+++ lib/trinidad_logging_extension/jars.rb
@@ -5,6 +5,7 @@ module Trinidad
     module LoggingJars
       SLF4J_VERSION = '1.6.1'
       LOG4J_VERSION = '1.2.16'
+      LOG4J_EXTRAS_VERSION = '1.1'
       LOGBACK_VERSION = '0.9.29'
 
       def require_common_jars
@@ -17,6 +18,7 @@ module Trinidad
         require "slf4j-log4j12-#{SLF4J_VERSION}.jar"
         require "log4j-#{LOG4J_VERSION}"
         require "jul-to-slf4j-#{SLF4J_VERSION}.jar"
+        require "apache-log4j-extras-#{LOG4J_EXTRAS_VERSION}.jar"
       end
 
       def require_logback_jars
